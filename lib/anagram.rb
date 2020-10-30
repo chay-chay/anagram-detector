9aef30b8a183d41981eea2ce60599404320d23f9
Chayanit McCormick <chayanit.chaisri@gmail.com>
2020-10-30 12:23:18 -0400

    commit tree 835ca50d59487efe35006cc98afed13fbec3dafb

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :name
+    def initialize(words)
+    @name = words
+    end
+    def match(array)
+        array.select {|x| x.split("").sort == @name.split("").sort}
+    
+end
+end
